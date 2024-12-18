5b06fbb39be5005556a332a9c30fac9d6fe4fb04
Netsanet-dev <Netsanetsolomon41@gmail.com>
2024-12-18 21:25:46 +0300

    Adding match case calculator file

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,18 @@
+# Prompt the user the enter a number and choose operations
+number1 = int(input("Enter the firstt number: "))
+number2 = int(input("Enter the second number: "))
+operation = str(input("Choose the operation (+, -, *, /): "))
+
+# Performing calculations
+match operation:
+    case "+":
+        print(number1 + number2)
+    case "-":
+        print(number1 - number2)
+    case "*":
+        print(number1 * numebr2)
+    case "/":
+        try:
+           print(number1 / number2)
+        except ZeroDivisionError:
+            print("Cannot divide by zero.")
