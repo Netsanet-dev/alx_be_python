f5ca63a87a2146534017348752f82b700741c501
Netsanet-dev <Netsanetsolomon41@gmail.com>
2024-12-22 23:56:27 +0300

    Update daily reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,9 +3,16 @@ priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
 match priority:
-    case "high" if time_bound == "yes":
-        print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
-    case "low" if time_bound == "no":
-        print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
+    case "high":
+        priority = f"'{task}' is a {priority} priority "
+    case "medium":
+        priority = f"'{task}' is a {priority} priority "
+    case "low":
+        priority = f"'{task}' is a {priority} priority task. "
     case _:
         print('Please enter the correct format')
+
+if time_bound == "yes":
+    print("Reminder: "+priority +" that requires immediate attention today!")
+else:
+    print(f"Note: {priority} Consider compeleting it when you have free time.")
