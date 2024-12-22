9483b0a18ea3da3d74d3d78dc7c3f3ac7ceddf81
Netsanet-dev <Netsanetsolomon41@gmail.com>
2024-12-22 23:10:35 +0300

    Adding daily reminder file

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,19 @@
 task = input("Enter your task: ")
-priority = input("Priority (high/medium/low: ")
+priority = input("Priority (high/medium/low): ")
 time = input("Is it time-bound? (yes/no): ")
 
-match task:
-    case
+match (priority, time):
+    case ('high', 'yes'):
+        print(f"'{task}' is a {priority} priority task that requires immediate attention today!")
+    case ('medium', 'yes'):
+        print(f"'{task}' is a {priority} priority task that requires medium attention today!")
+    case ('low', 'yes'):
+        print(f"'{task}' is a {priority} priority task that requires low attention today!")
+    case ('high', 'no'):
+        print(f"'{task}' is a {priority} priority task that requires attention but complete when you have free time.")
+    case ('medium', 'no'):
+        print(f"'{task}' is a {priority} priority task that requiers medium attention, complete when you have free time.")
+    case ('low', 'no'):
+        print(f"'{task}' is a {priority} priority task. Consider completing it when you have free time.")
+    case _:
+        print('Please enter the correct format')
