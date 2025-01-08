5b14af5aaeaa892e5f0d2c32dc0fb18cdbb4b057
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-01-08 22:09:32 +0300

    Changing Match case to if else statement

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,15 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            try:
-                return num1/num2
-            except ZeroDivisionError:
-                print(f"The denominator must be greater than zero")
-        case _:
-            print("Please enter the correct number")
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        try:
+            return num1/num2
+        except ZeroDivisionError:
+                print("The denominator must be greater than zero")
+    else:
+        print("Please enter the correct number")
     
