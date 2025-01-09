3ae215a6790aaa5509b63a1e18c29a02b5304dd8
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-01-09 14:46:01 +0300

    shopping list manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,23 +1,24 @@
 def display_menu():
+    print("=======================")
     print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
     print("4. Exit")
-
+    print("=======================")
+    
 def main():
     shopping_list = []
     while True:
         display_menu()
         choice = int(input("Enter your choice: "))
-
         if choice == 1:
             # Prompt for and add an item
-            item = input("Enter the item to add:  ")
+            item = str(input("Enter the item to add:  "))
             shopping_list.append(item)
         elif choice == 2:
             # Prompt for and remove an item
-            item =input("Enter the item you want to remove. ")
+            item = str(input("Enter the item to remove. "))
             if shopping_list == []:
                 print("The list is Empty.")
             elif item not in shopping_list:
