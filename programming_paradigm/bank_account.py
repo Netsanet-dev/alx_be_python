f3617ff5e7e0e21e1b86bfad8e05720a21a38c11
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-01-19 21:15:38 +0300

    Bank Account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,34 +1,15 @@
 class BankAccount:
     def __init__(self, balance):
        self.account_balance = balance
-       balance = self.account_balance
        
     def deposit(self, amount):
         self.account_balance += amount
         
     def withdraw(self, amount):
-        assert self.account_balance - amount < 0, False
-        self.account_balance -= amount
+        if self.account_balance - amount < 0:
+            return False
+        else:
+            self.account_balance -= amount
             
     def display_balance(self):
        print(f"Current Balance: ${self.account_balance}")
-
-
-if __name__ == "__main__":
-    from bank_account import BankAccount
-    while True:
-        command = input("command: ")
-        amount = int(input("number: "))
-        account = BankAccount(100)  # Example starting balance
-        if command == "deposit" and amount is not None:
-            account.deposit(amount)
-            print(f"Deposited: ${amount}")
-        elif command == "withdraw" and amount is not None:
-            if account.withdraw(amount):
-                print(f"Withdrew: ${amount}")
-            else:
-                print("Insufficient funds.")
-        elif command == "display":
-            account.display_balance()
-        elif command == 'q':
-            break
