93b0c75f7501f39ed3325f4a8e584f20a3dcec23
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-01-19 16:34:15 +0300

    Bank account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,38 +1,17 @@
 class BankAccount:
-    account_balance = 0
+    balance = 0
     def __init__(self, account_balance):
        self.account_balance = account_balance
        
     def deposit(self, amount):
-        BankAccount.account_balance = self.account_balance + amount
-        print(f"{BankAccount.account_balance} deposited")
-        return self.account_balance
+        BankAccount.balance = self.account_balance + amount
+        print(f"{self.account_balance} deposited")
+        return self.balance
     
     def withdraw(self, amount):
-        BankAccount.account_balance = self.account_balance - amount
+        BankAccount.balance = self.account_balance - amount
         print(f"{self.account_balance} deposited")
-        return self.account_balance
+        return self.balance
     
     def display_balance(self):
-       print(f"Current Balance: ${BankAccount.account_balance}")
-       
-       
-# if __name__ == "__main__":
-#     import sys
-#     from bank_account import BankAccount
-#     while True:
-#         command = input("command: ")
-#         amount = int(input("number: "))
-#         account = BankAccount(100)  # Example starting balance
-#         if command == "deposit" and amount is not None:
-#             account.deposit(amount)
-#             print(f"Deposited: ${amount}")
-#         elif command == "withdraw" and amount is not None:
-#             if account.withdraw(amount):
-#                 print(f"Withdrew: ${amount}")
-#             else:
-#                 print("Insufficient funds.")
-#         elif command == "display":
-#             account.display_balance()
-#         elif command == 'q':
-#             break
+       print(f"Current Balance: ${self.balance}")
