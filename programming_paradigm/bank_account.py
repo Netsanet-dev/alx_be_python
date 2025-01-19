e0a4b6b2fe2fd7b7527ecd83f25b7b61ed5307d7
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-01-19 15:06:37 +0300

    Bank Account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,20 +1,38 @@
 class BankAccount:
-    # current_balance = 0
+    account_balance = 0
     def __init__(self, account_balance):
        self.account_balance = account_balance
-
+       
     def deposit(self, amount):
-        current_balance = self.account_balance + amount
-        self.account_balance = current_balance
-        return self.account_balance
+        BankAccount.account_balance = self.account_balance + amount
+        print(f"{BankAccount.account_balance} deposited")
+        return BankAccount.account_balance
     
     def withdraw(self, amount):
-        current_balance = self.account_balance - amount
-        self.account_balance = current_balance
-        return self.account_balance
-    
+        BankAccount.account_balance = self.account_balance - amount
+        print(f"{self.account_balance} deposited")
+        return BankAccount.account_balance
     
     def display_balance(self):
-       current_balance = self.account_balance
-       print(f"Current Balance: ${current_balance}")
-
+       print(f"Current Balance: ${BankAccount.account_balance}")
+       
+       
+# if __name__ == "__main__":
+#     import sys
+#     from bank_account import BankAccount
+#     while True:
+#         command = input("command: ")
+#         amount = int(input("number: "))
+#         account = BankAccount(100)  # Example starting balance
+#         if command == "deposit" and amount is not None:
+#             account.deposit(amount)
+#             print(f"Deposited: ${amount}")
+#         elif command == "withdraw" and amount is not None:
+#             if account.withdraw(amount):
+#                 print(f"Withdrew: ${amount}")
+#             else:
+#                 print("Insufficient funds.")
+#         elif command == "display":
+#             account.display_balance()
+#         elif command == 'q':
+#             break
