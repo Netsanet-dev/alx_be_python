07c44a51ed15ecf5c90c81b5bc6322a532e5ffa2
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-01-19 21:24:06 +0300

    Bank Account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,12 +4,14 @@ class BankAccount:
        
     def deposit(self, amount):
         self.account_balance += amount
+        return self.account_balance
         
     def withdraw(self, amount):
         if amount < 0 or amount > self.account_balance:
             return False
         else:
             self.account_balance -= amount
+            return self.account_balance
             
     def display_balance(self):
        print(f"Current Balance: ${self.account_balance:.2f}")
