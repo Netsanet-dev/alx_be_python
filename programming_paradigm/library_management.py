f1b150ebc1a671e69014a6c1f0db37e665ca20fa
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-01-20 00:01:28 +0300

    library management

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,1 +1,24 @@
-
+class Book:
+    def __init__(self,title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+        
+class Library(Book):
+    def __init__(self):
+        super().__init__()
+        self._books = []
+          
+    def add_book(self, *args):
+        book = self._books.append(Book(args))
+        return f'addbook {book}'
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                return f'{self.title} is'
+        pass
+    def return_book(self, title):
+        pass
+    def list_available_books(self):
+        pass
+    
