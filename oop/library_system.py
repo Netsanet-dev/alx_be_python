e8b7800cff21575bfd25c8f49d59e3c4f8279bed
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-01-26 23:17:56 +0300

    Library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,6 +2,9 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+    
+    def __str__(self):
+        return f'Book class'
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count: int):
